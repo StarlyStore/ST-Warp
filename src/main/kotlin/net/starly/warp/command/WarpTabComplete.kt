f75a54e17278bb0e6557a320f172f112ef65e841
idkNicks <yangdaeyeong0808@gmail.com>
2023-01-11 08:22:35 +0900

    Feat: TabComplete Permissions

--- src/main/kotlin/net/starly/warp/command/WarpTabComplete.kt
+++ src/main/kotlin/net/starly/warp/command/WarpTabComplete.kt
@@ -16,48 +16,53 @@ class WarpTabComplete : TabCompleter {
         label: String,
         args: Array<out String>
     ): MutableList<String> {
+
         var list: MutableList<String> = mutableListOf()
         var player: Player = sender as Player
 
         if (args.size == 1) {
 
+            StringUtil.copyPartialMatches(args[0], listOf("이동"), list)
+
             if (player.isOp) {
                 StringUtil.copyPartialMatches(args[0], listOf("강제이동", "생성", "제거", "목록", "리로드"), list)
-            } else {
-                StringUtil.copyPartialMatches(args[0], listOf("이동"), list)
             }
+
         } else if (args.size == 2) {
+
+            if (args[0].equals("이동")) {
+                val config: Config = Config("warp/", instance)
+                for (warp: String in config.fileListName()) {
+                    list.add(warp)
+                }
+            }
 
             if (player.isOp) {
                 if (args[0].equals("제거")) {
                     val config: Config = Config("warp/", instance)
-//                for (warp: String in config.fileListName()) {
-//                    list.add(warp)
-//                }
-                }
+                    for (warp: String in config.fileListName()) {
+                        list.add(warp)
+                    }
 
-            } else {
-                if (args[0].equals("이동")) {
-                    val config: Config = Config("warp/", instance)
-//                for (warp: String in config.fileListName()) {
-//                    list.add(warp)
-//                }
+
                 } else if (args[0].equals("강제이동")) {
                     for (player in instance!!.server.onlinePlayers) {
                         list.add(player.name)
                     }
+
                 } else if (args[0].equals("생성")) {
                     list.add("<이름>")
                 }
+
             }
 
         } else if (args.size == 3) {
-            if(player.isOp) {
+            if (player.isOp) {
                 if (args[0].equals("강제이동")) {
                     val config: Config = Config("warp/", instance)
-//                for (warp: String in config.fileListName()) {
-//                    list.add(warp)
-//                }
+                    for (warp: String in config.fileListName()) {
+                        list.add(warp)
+                    }
                 }
             }
         }
